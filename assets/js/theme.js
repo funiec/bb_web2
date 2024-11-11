1bfe1f4cd3e48917549f95455b405c99e3a21108
funiec <funiec2024.mr@gmail.com>
2024-11-11 12:07:33 +0100

    Initial commit

---
+++ assets/js/theme.js
@@ -0,0 +1,15 @@
+import {
+    docReady
+} from './utils';
+import navbarInit from './bootstrap-navbar';
+import detectorInit from './detector';
+import navbarDarkenOnScroll from './navbar-darken-on-scroll'
+
+
+// /* -------------------------------------------------------------------------- */
+// /*                            Theme Initialization                            */
+// /* -------------------------------------------------------------------------- */
+
+docReady(navbarInit);
+docReady(detectorInit);
+docReady(navbarDarkenOnScroll);
